c564fbd1bae62e1f06f9768dadc278f5cfc0633c
Esgagos <135175639+Esgagos@users.noreply.github.com>
2023-12-22 00:25:19 +0000

    Added basic javaScript and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Welcom to the poem generator");
+const makeCall = (response) => {
+  console.log(response);
+};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log("done");
+  const poem = document.querySelector("#poem");
+  new Typewriter(poem, {
+    strings: "generating poem",
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
+
+  //   poem.innerHTML = "generating poem";
+  //   const wordInput = document.querySelector(".word-input");
+  //   const context =
+  //     "please provide a classic poem with the name of it and without any introduction to it";
+  //   const prompt = `please generate a poem`;
+  //   const apiKey = "6e77343taf210f7060a5ae1ab4ao9183";
+  //   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=?${apiKey}`;
+  //   axios.get(apiUrl).then(makeCall);
+};
+const text = document.querySelector("#text-input");
+text.addEventListener("submit", handleSubmit);
