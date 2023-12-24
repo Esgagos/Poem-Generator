8fcf39d959acf2cd24f7a1747a6e53473e419eba
Esgagos <135175639+Esgagos@users.noreply.github.com>
2023-12-24 01:17:31 +0000

    Integrated API and style the response

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const makeCall = (response) => {
   console.log(response);
+  const poem = document.querySelector("#poem");
+  new Typewriter(poem, {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
 };
 const handleSubmit = (event) => {
   event.preventDefault();
   console.log("done");
-  const poem = document.querySelector("#poem");
-  new Typewriter(poem, {
-    strings: "generating poem",
-    autoStart: true,
-    cursor: null,
-    delay: 50,
-  });
-
-  //   poem.innerHTML = "generating poem";
-  //   const wordInput = document.querySelector(".word-input");
-  //   const context =
-  //     "please provide a classic poem with the name of it and without any introduction to it";
-  //   const prompt = `please generate a poem`;
-  //   const apiKey = "6e77343taf210f7060a5ae1ab4ao9183";
-  //   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=?${apiKey}`;
-  //   axios.get(apiUrl).then(makeCall);
+  const wordInput = document.querySelector(".word-input");
+  const prompt = `generate a poem with ${wordInput.value}`;
+  const context =
+    "please provide a classic short poem with its title in basic html without an introduction at the begining of it. Please add the author of the poem at the end inside <em> tags";
+  const apiKey = "6e77343taf210f7060a5ae1ab4ao9183";
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(makeCall);
 };
 const text = document.querySelector("#text-input");
 text.addEventListener("submit", handleSubmit);
