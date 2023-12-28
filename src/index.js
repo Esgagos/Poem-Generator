9050832b47f221e94deac04d51273bd57167824c
Esgagos <135175639+Esgagos@users.noreply.github.com>
2023-12-28 00:43:52 +0000

    Added blink effect to the generating message  and change the poem of the landing page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const makeCall = (response) => {
   console.log(response);
+
   const poem = document.querySelector("#poem");
   new Typewriter(poem, {
     strings: response.data.answer,
@@ -11,10 +12,12 @@ const makeCall = (response) => {
 const handleSubmit = (event) => {
   event.preventDefault();
   console.log("done");
+  const poem = document.querySelector("#poem");
+  poem.innerHTML = "<div class='blink'>Generating poem... 🙄</div>";
   const wordInput = document.querySelector(".word-input");
   const prompt = `generate a poem with ${wordInput.value}`;
   const context =
-    "please provide a classic short poem with its title in basic html without an introduction at the begining of it. Please add the author of the poem at the end inside <em> tags";
+    "please provide a classic short poem with its title in basic html. Don't provide any introduction at the begining of it. Please add the author of the poem AT THE END inside <em> tags";
   const apiKey = "6e77343taf210f7060a5ae1ab4ao9183";
   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(makeCall);
