23363a0e9872ddabb8b987a378be8ebb288a12aa
Esgagos <135175639+Esgagos@users.noreply.github.com>
2023-12-28 00:54:17 +0000

    Added a placeholder and change the context

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const handleSubmit = (event) => {
   const wordInput = document.querySelector(".word-input");
   const prompt = `generate a poem with ${wordInput.value}`;
   const context =
-    "please provide a classic short poem with its title in basic html. Don't provide any introduction at the begining of it. Please add the author of the poem AT THE END inside <em> tags";
+    "please provide a very short poem in basic html. Don't provide any introduction at the begining of it. Please add the author of the poem AT THE END inside <em> tags";
   const apiKey = "6e77343taf210f7060a5ae1ab4ao9183";
   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(makeCall);
